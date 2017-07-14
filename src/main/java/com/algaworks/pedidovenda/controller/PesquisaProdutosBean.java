7b0cf8e30d700fb0049642132151a6507668ec8a
lcristianolopes <lcristianoprogrammer@gmail.com>
2017-07-14 17:22:53 -0300

    Criacao de cadastros e pedidos

--- src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
+++ src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
@@ -1,18 +1,19 @@
 package com.algaworks.pedidovenda.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-
 @ManagedBean
 @RequestScoped
 public class PesquisaProdutosBean {
 
-	private List<Integer> produtosFiltrados = null;
+	private List<Integer> produtosFiltrados;
 
 	public PesquisaProdutosBean() {
+		produtosFiltrados = new ArrayList<>();
 		for (int i = 0; i < 50; i++) {
 			produtosFiltrados.add(i);
 		}
